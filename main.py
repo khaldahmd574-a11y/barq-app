805d3c03dad7cb5afe1159de08433fdd16fe1529
khaldahmd574-a11y <khaldahmd574@gmail.com>
2026-09-12 03:23:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,15 @@ API_ID = int(os.environ.get("TELEGRAM_API_ID", 39120728))
 API_HASH = os.environ.get("TELEGRAM_API_HASH", "1deec8393ce5aa05c54c0c7e280377d4")
 BOT_TOKEN = "8782796916:AAEe9YRkzbfm3F5e9rj49iHfDS0wRTnVmmo"
 
-TARGET_USERS = ["shaybq", "Waaaaaaa33", "abood1317", "fs_990"]
+# قائمة المشتركين (تم إضافة الحسابين الجديدين هنا)
+TARGET_USERS = [
+    "shaybq", 
+    "Waaaaaaa33", 
+    "abood1317", 
+    "fs_990", 
+    "Ndhhyfvvjkcd", 
+    "mushtaq_28"
+]
 
 RAW_KEYWORDS = [
     "جيزان", "جازان", "بيش", "الدرب", "صبيا", "ضمد", "الضبيه", "الظبيه", "مزهره", 
